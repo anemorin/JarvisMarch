5e39de7a809ee9b72e0ce4d916fdaba780d76ec7
anemorin <arknahon@gmail.com>
2023-04-02 10:50:05 +0300

    Add sem work

---
+++ jarvisMarch/Generator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Generator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter
+                ("C:\\Users\\deter\\RiderProjects\\jarvisMarch\\Generator\\values.txt");
+            var rnd = new Random();
+            var value = new List<string>();
+            value.AddRange(Make(100, rnd));
+            for (int i = 0; i < 100; i++)
+            {
+                var strs = value[i];
+                sw.WriteLine(strs);
+            }
+            sw.Close();
+        }
+
+        public static List<string> Make(int countStr, Random rnd)
+        {
+            var countElem = 100;
+            var strArray = new List<string>();
+            for (int i = 0; i < countStr; i++)
+            {
+                var str = new StringBuilder();
+                for (int j = 0; j < countElem; j++)
+                {
+                    var x = rnd.Next(-1000, 1000).ToString();
+                    var y = rnd.Next(-1000, 1000).ToString();
+                    str.Append(x + "." + y + " ");
+                }
+                strArray.Add(str.ToString());
+                countElem += 100;
+            }
+
+            return strArray;
+        }
+    }
+}
---
+++ jarvisMarch/jarvisMarch/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace jarvisMarch
+{
+    internal class Program
+    {
+        public static int iteration; 
+        public static void Main(string[] args)
+        {
+            var streamReader = 
+                new StreamReader("C:\\Users\\deter\\RiderProjects\\jarvisMarch\\Generator\\values.txt");
+            
+            string line;
+            var lineList = new List<string>();
+            while ((line = streamReader.ReadLine()) != null)
+                lineList.Add(line);
+            
+            streamReader.Close();
+            
+            var times = new List<TimeSpan>();
+            var iterations = new List<int>();
+
+            var pointsList = new List<Point>();
+            for (int j = 0; j < lineList.Count; j++)
+            {
+                foreach (var item in lineList[j].Split(' '))
+                {
+                    var m = item.Split('.');
+                    if (string.IsNullOrEmpty(m[0]))
+                        break;
+                    pointsList.Add(new Point(int.Parse(m[0]), int.Parse(m[1])));
+                }
+                
+                Stopwatch x = new Stopwatch();
+                x.Start();
+                var p = JarvisMarch(pointsList);
+                x.Stop();
+                
+                times.Add(x.Elapsed);
+                iterations.Add(iteration);
+                
+                Console.WriteLine((j + 1) + ": complete");
+            }
+            
+            var streamWriter =
+                new StreamWriter("C:\\Users\\deter\\RiderProjects\\jarvisMarch\\jarvisMarch\\result.txt");
+            for (int i = 0; i < times.Count; i++)
+            {
+                streamWriter.WriteLine("Time is {0:00}.{1} iterations: {2}", 
+                    times[i].Seconds,  times[i].TotalMilliseconds, iterations[i]);
+            }
+            streamWriter.Close();
+        }
+        
+        private static int Rotate(Point p1, Point p2, Point p3)
+        {
+            int val = (p2.Y - p1.Y) * (p3.X - p2.X) -
+                      (p2.X - p1.X) * (p3.Y - p2.Y);
+      
+            if (val == 0) return 0; 
+            return (val > 0)? 1: 2; 
+        }
+        
+        private static List<Point> JarvisMarch(List<Point> points)
+        {
+            iteration = 0;
+            List<Point> hull = new List<Point>();
+            var n = points.Count;
+            int startPointId = 0;
+            for (int i = 1; i < n; i++)
+            {
+                iteration++;
+                if (points[i].X < points[startPointId].X)
+                    startPointId = i;
+            }
+
+            int currentPointId = startPointId;
+            do
+            {
+                iteration++;
+                hull.Add(points[currentPointId]);
+                
+                var nextPointId = (currentPointId + 1) % n;
+              
+                for (int i = 0; i < n; i++)
+                    if (Rotate(points[currentPointId], points[i], points[nextPointId]) == 2)
+                        nextPointId = i;
+                currentPointId = nextPointId;
+      
+            } while (currentPointId != startPointId);
+            
+            hull.Add(points[startPointId]);
+            return hull;
+        }
+    }
+
+    internal class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
